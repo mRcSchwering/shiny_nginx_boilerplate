2b09cc3280da1acd29292d816f757285c0fc0298
Marc Schwering <schweringmarc01@gmail.com>
2017-12-09 21:56:20 +0100

    Nix

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # butterbirne
+
+
+1. Statistik auf Daten machen
+  - welche Daten brauch ich
+  - welche Quellen gibts dafür
+  - schon mal einfache App bauen
+  - dockern?
+2. Datenbanken spiegeln
+  - Aufbau der Datenbanken
+  - Update Prozeduren
+  - API?
+  - docker?
---
+++ butterbirne.py
@@ -0,0 +1,17 @@
+"""Short Description.
+
+Detailed Description
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+
+FLAGS = None
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    print(main())
